f39b4633b375e7eb5e517eb49090a47caa6d0a50
Dhanoj01 <149933983+Dhanoj01@users.noreply.github.com>
2025-11-12 15:16:44 +0530

    Update ArrayLength.java

--- ArrayLength.java
+++ ArrayLength.java
@@ -2,4 +2,12 @@ package Array;
 
 public class ArrayLength {
 
+  public static void main(String args[])
+	{
+		int[] arr = {1,3,5,7,9};
+		int len = arr.length; 
+		System.out.println("Length is : " + len);
+	}
+
 }
+
