bb86ebaa6cfc8b6aace883c33e6fa25c9db4ec90
Dhanoj01 <149933983+Dhanoj01@users.noreply.github.com>
2025-11-29 05:50:38 -0800

    check vowel is present or not in String

---
+++ CheckVowel.java
@@ -0,0 +1,15 @@
+package String;
+
+public class CheckVowel {
+
+	public static void main(String args[])
+	{
+		System.out.println(stringContainsVowel("Hello"));
+		System.out.println(stringContainsVowel("TV"));
+	}
+	
+	public static boolean stringContainsVowel(String input) {
+		
+		return input.toLowerCase().matches(".*[aeiou].*");
+	}
+}
