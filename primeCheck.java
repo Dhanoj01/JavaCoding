851f77204c1e719a92069ecca5f793f9a9bbdf9a
Dhanoj01 <149933983+Dhanoj01@users.noreply.github.com>
2025-12-05 08:21:11 -0800

    Add files via upload

---
+++ primeCheck.java
@@ -0,0 +1,37 @@
+package Array;
+
+public class primeCheck {
+
+	public static void main(String args[])
+	{
+		System.out.println(isprime(78));
+		System.out.println(isprime(50));
+	}
+	
+	public static boolean isprime(int n)
+	{
+		if(n==1 || n==0)
+		{
+			return false;
+		}
+		if(n==2)
+		{
+			return true;
+		}
+		
+		else {
+			
+			for(int i = 2 ; i < n/2 ; i++)
+			{
+				if(n%i==0)
+				{
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
+	
+	
+}
